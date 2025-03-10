c82354c161ee63db5790708bd8d36262b95cf11d
majuszl <167575638+majuszl@users.noreply.github.com>
2025-03-10 17:09:39 -0300

    Delete Projetopython directory

--- Projetopython/config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = 'CHAVE_SECRETA_SECRETISSIMA' #VARIÁVEL GLOBAL - chave secreta que protege meu projeto
-DEBUG = True
-DB_HOST = 'localhost' #por que está aqui na minha máquina local, VARIÁVEL GLOBAL
-DB_NAME = r'C:\Users\Aluno\Downloads\BANCO (4)\BANCO.FDB' #caminho do meu banco, o "r" formata para ser um caminho, VARIÁVEL GLOBAL
-DB_USER = 'sysdba' # VARIÁVEL GLOBAL
-DB_PASSWORD = 'sysdba' # VARIÁVEL GLOBAL
